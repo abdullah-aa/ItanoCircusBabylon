b596dab3ae9801d5fc969b56f1377b3ff2135dc1
Abdullah Akram Ansari <abdullaha90@gmail.com>
2025-06-21 23:35:44 -0400

    Adds ESLint and Prettier configurations, along with formatting and linting scripts to enforce code quality standards.

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'prettier/prettier': 'warn',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
+  ignorePatterns: ['dist', 'node_modules', '*.js', '!.eslintrc.js'],
+};
